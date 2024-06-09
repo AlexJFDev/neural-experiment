ccea4924aa0efdfe3af58bd53dd084647066fbc4
alexander.fuhrig.23 <alexander.fuhrig.23@cnu.edu>
2024-06-09 00:58:29 -0400

    Updated column reduction

--- Minesweeper/matrix_functions.py
+++ Minesweeper/matrix_functions.py
@@ -3,14 +3,34 @@ import numpy as np
 def reduce_matrix(matrix):
     print(matrix[:, 0])
 
-def reduce_column(matrix, starting_row, column_index):
+def reduce_column(matrix: np.ndarray, starting_row: int, column_index: int):
     column = (matrix[:, column_index])
-    pass
+    row_index = starting_row
+    while row_index < column.shape[0] and column[row_index] == 0: # Find the first non-zero row in the column
+        row_index += 1
+
+    matrix[[row_index, starting_row]] = matrix[[starting_row, row_index]] # Swap the non-zero row with the starting row
+
+    row = matrix[starting_row]
+    if row[column_index] != 1:
+        row //= row[column_index] # If the column's value in the row is not 1 make it 1
+
+    for row_i in matrix[starting_row + 1:]: # Reduce the rest of the rows
+        if (row_i[column_index] != 0):
+            coefficient = row_i[column_index]
+            row_i -= coefficient * row
+
+    for row_i in matrix[:starting_row]:
+        if (row_i[column_index] != 0):
+            coefficient = row_i[column_index]
+            row_i -= coefficient * row
+
+    
 
 if __name__ == "__main__":
     input_matrix = np.array(
         [
-            #A  B  C  D  E  F  G  H  I  J  K  coefficient
+            #A  B  C  D  E  F  G  H  I  J  K  constants
             [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1], # 1
             [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1], # 2
             [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1], # 3
@@ -22,7 +42,7 @@ if __name__ == "__main__":
     )
     output_matrix = np.array(
         [
-            #A  B  C  D  E  F  G  H  I  J  K  coefficient
+            #A  B  C  D  E  F  G  H  I  J  K  constants
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1, 0], # 1
             [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1], # 2
             [0, 0, 1, 0, 0,-1, 1, 0, 0, 0, 0, 0], # 3
@@ -32,4 +52,7 @@ if __name__ == "__main__":
             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1], # 7
         ]
     )
-    reduce_matrix(input_matrix)
+    reduce_column(input_matrix, 0, 0)
+    reduce_column(input_matrix, 1, 1)
+    reduce_column(input_matrix, 2, 2)
+    print(input_matrix)
