2675de1dcdd91fdb6253e51795ddba2229737a61
alexander.fuhrig.23 <alexander.fuhrig.23@cnu.edu>
2024-06-09 01:16:05 -0400

    Finished reduce matrix

--- Minesweeper/matrix_functions.py
+++ Minesweeper/matrix_functions.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def reduce_matrix(matrix):
-    print(matrix[:, 0])
-
-def reduce_column(matrix: np.ndarray, starting_row: int, column_index: int):
+def reduce_matrix(matrix: np.ndarray, starting_row: int, column_index: int):
     column = (matrix[:, column_index])
     row_index = starting_row
     while row_index < column.shape[0] and column[row_index] == 0: # Find the first non-zero row in the column
         row_index += 1
+        if row_index == column.shape[0]:
+            reduce_matrix(matrix, starting_row, column_index + 1) # No reduction is possible in this column
+            return
 
     matrix[[row_index, starting_row]] = matrix[[starting_row, row_index]] # Swap the non-zero row with the starting row
 
@@ -25,7 +25,9 @@ def reduce_column(matrix: np.ndarray, starting_row: int, column_index: int):
             coefficient = row_i[column_index]
             row_i -= coefficient * row
 
-    
+    if (starting_row == matrix.shape[0] - 1):
+        return
+    reduce_matrix(matrix, starting_row + 1, column_index + 1)
 
 if __name__ == "__main__":
     input_matrix = np.array(
@@ -45,14 +47,14 @@ if __name__ == "__main__":
             #A  B  C  D  E  F  G  H  I  J  K  constants
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1, 0], # 1
             [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1], # 2
-            [0, 0, 1, 0, 0,-1, 1, 0, 0, 0, 0, 0], # 3
-            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0], # 4
+            [0, 0, 1, 0, 0,-1, 0, 0, 0, 0, 0, 0], # 3
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], # 4
             [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1], # 5
             [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0], # 6
             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1], # 7
         ]
     )
-    reduce_column(input_matrix, 0, 0)
-    reduce_column(input_matrix, 1, 1)
-    reduce_column(input_matrix, 2, 2)
+    reduce_matrix(input_matrix, 0, 0)
     print(input_matrix)
+    print()
+    print(output_matrix)
